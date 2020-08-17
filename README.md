227e7fdcc3f801766f569b10574ddc9e6079b023
Joel <joelmaxw311@gmail.com>
2020-08-17 11:02:21 -0700

    Added interactive execution mode.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # BarotraumaSaveEditor
 Modify .save files of the game Barotrauma
+
+# Usage
+    python save_editor.py [[-h] {import,export} save_file_path]
+
+## Non-Interactive Mode
+Run the tool from the shell, providing parameters as command arguments.
+
+### Show command help
+    python save_editor.py -h
+
+### Decompress a .save file
+Extract gamesession.xml and .sub files from a .save file to ./tmp/import/
+
+    python save_editor.py import "path/to/save/file.save"
+
+Edit gamesession.xml and add copy .sub files to ./tmp/import/ to modify the save data.
+
+### Recompress files to a .save file
+Compress modified save data from ./tmp/import/ to a Barotrauma .save file.
+
+    python save_editor.py export "path/to/save/file.save"
+
+## Interactive mode
+Executing the save_editor.py directly, without providing command-line arguments, will run the tool in interactive mode.
+In interactive mode, the program will prompt the user to enter each parameter.
+
+    > ./save_editor.py
+    import or export:
+    import
+    file path:
+    path/to/save/file.save
--- save_editor.py
+++ save_editor.py
@@ -3,6 +3,7 @@ import sys
 import os
 import argparse
 import shutil
+import logging
 
 
 OPERATION_STRINGS = ['import', 'export']
@@ -10,79 +11,124 @@ OPERATION_STRINGS = ['import', 'export']
 TEMPORARY_FILES_PATH = './tmp/'
 IMPORT_DIR_PATH = TEMPORARY_FILES_PATH + 'import/'
 EXPORT_DIR_PATH = TEMPORARY_FILES_PATH + 'export/'
-BACKUP_FILE_PATH = TEMPORARY_FILES_PATH + 'backup.save'
-
-XML_FILE_PATH = IMPORT_DIR_PATH + 'save.xml'
-BIN_FILE_PATH = IMPORT_DIR_PATH + 'save.bin'
-
-EXPORT_FILE_PATH = EXPORT_DIR_PATH + 'export.rawsave'
-EXPORT_SAVE_PATH = EXPORT_DIR_PATH + 'export.save'
 
 SIZEOF_INT = 4
 SIZEOF_CHAR = 2
 BYTE_ORDER = 'little'
 
-def parse_file(gz : gzip.GzipFile) -> (str, bytes):
-    # read file title
+
+# Read the next string value from GzipFile object "gz"
+def gzip_parse_string(gz):
     num_bytes = int.from_bytes(gz.read(SIZEOF_INT), byteorder=BYTE_ORDER)
-    print(num_bytes)
-    file_name : str = ""
+    s = ""
     for i in range(num_bytes):
-        file_name = file_name + chr(int.from_bytes(gz.read(SIZEOF_CHAR), byteorder=BYTE_ORDER))
-    # read file data
+        s = s + chr(int.from_bytes(gz.read(SIZEOF_CHAR), byteorder=BYTE_ORDER))
+    return s
+
+
+# Read the next block of bytes from GzipFile object "gz"
+def gzip_parse_bytes(gz):
     len_data = int.from_bytes(gz.read(SIZEOF_INT), byteorder=BYTE_ORDER)
-    print(f"File {file_name} ({len_data} bytes)")
     file_data = gz.read(len_data)
+    return file_data
+
+
+# Parse multiple files from GzipFile object "gz"
+def gzip_parse_file(gz):
+    # read file title
+    file_name = gzip_parse_string(gz)
+    # read file data
+    file_data = gzip_parse_bytes(gz)
     return file_name, file_data
 
 
-def extract_save(save_file, dir_out) -> dict:
+# Write string value "strval" to GzipFile object "gz"
+def gzip_write_string(gz, strval):
+    # write length of string
+    gz.write(int.to_bytes(len(strval), SIZEOF_INT, byteorder=BYTE_ORDER)) 
+    # write string characters
+    for c in strval: 
+        gz.write(int.to_bytes(ord(c), SIZEOF_CHAR, byteorder=BYTE_ORDER))
+
+
+# Write bytes of "bdata" to GzipFile object "gz"
+def gzip_write_bytes(gz, bdata):
+    gz.write(int.to_bytes(len(bdata), SIZEOF_INT, byteorder=BYTE_ORDER)) # write length of file data
+    gz.write(bdata) # write file data
+
+
+# Decompress and extract all files from a .save file
+# The .save file will contain one .xml and one or more .sub files.
+# Write imported files to ./tmp/import/
+def extract_save(save_file):
     file_map = {}
     with gzip.open(save_file, 'rb') as gz:
-        print(len(gz.peek(SIZEOF_INT)))
         while(len(gz.peek(SIZEOF_INT)) >= SIZEOF_INT):
-            file_name, file_data = parse_file(gz)
+            file_name, file_data = gzip_parse_file(gz)
             file_map[file_name] = file_data
-            with open(f"{dir_out}/{file_name}", 'wb+') as f:
-                f.write(file_data) # write decompressed data to file_out
-            print(len(gz.peek(SIZEOF_INT)))
     return file_map
 
 
+# Compress and write all files in directory "dir_in" to one .save file "save_out"
 def compress_save(dir_in, save_out):
     with gzip.open(save_out, 'wb+') as gz: 
         for file_in in os.listdir(dir_in):
             with open(f"{dir_in}/{file_in}", 'rb') as f:
                 bdata = f.read() # read uncompressed data from file_in
-            gz.write(int.to_bytes(len(os.path.basename(file_in)), SIZEOF_INT, byteorder=BYTE_ORDER)) # write length of file name
-            for c in os.path.basename(file_in): # write file name
-                gz.write(int.to_bytes(ord(c), SIZEOF_CHAR, byteorder=BYTE_ORDER))
-            gz.write(int.to_bytes(len(bdata), SIZEOF_INT, byteorder=BYTE_ORDER)) # write length of file data
-            gz.write(bdata) # write file data
+            file_name = os.path.basename(file_in)
+            gzip_write_string(gz, file_name)
+            gzip_write_bytes(gz, bdata)
 
 
+# Validate a .save file "save_out" against files in directory "dir_in".
+# Ensure that the file names and decompressed file contents of the .save file "save_out" match the 
+# names and contents of files in directory "dir_in".
+# Returns True if the .save is valid, or False if the .save is invalid.
+def validate_save(dir_in, save_out):
+    validated_files : dict = extract_save(EXPORT_SAVE_PATH) # extract files data from .save file
+    for file_in in os.listdir(dir_in): # for each file in directory "dir_in"
+        if not file_in in validated_files: # file name not found in .save archive
+            return False # .save file is invalid: missing file
+        with open(f"{dir_in}/{file_in}", 'rb') as f:
+            bdata = f.read() # read uncompressed data from file_in
+        if not bdata == validated_files[file_in]: # raw file data does not match data extracted from .save
+            return False # .save file is invalid: inconsistent file contents
+    return True # .save file is valid
+
+
+# Decompress files from .save file "save_file" to separate files in directory "dir_in"
 def import_save(save_file):
+    # create IMPORT_DIR_PATH if it does not exist
     if not os.path.isdir(IMPORT_DIR_PATH):
-        os.mkdir(IMPORT_DIR_PATH) # create IMPORT_DIR_PATH if it does not exist
-    save_data : bytes = extract_save(save_file, IMPORT_DIR_PATH) # extract the .save file
+        os.makedirs(IMPORT_DIR_PATH) 
+    save_data = extract_save(save_file) # extract files data fom the .save file
+    # write decompressed files
+    for file_name, file_data in save_data.items():
+        print(f"{file_name}: {len(file_data) if file_data is not None else file_data} bytes")
+        with open(f"{IMPORT_DIR_PATH}/{file_name}", 'wb+') as f:
+            f.write(file_data) # write decompressed data to imported file
 
 
+# Compress and write all files in directory "dir_in" to one .save file "save_file"
 def export_save(save_file):
+    # create EXPORT_DIR_PATH if it does not exist
     if not os.path.isdir(EXPORT_DIR_PATH):
-        os.mkdir(EXPORT_DIR_PATH) # create EXPORT_DIR_PATH if it does not exist
-    shutil.copyfile(save_file, BACKUP_FILE_PATH) # back up the .save file
-    compress_save(IMPORT_DIR_PATH, EXPORT_SAVE_PATH) # compress to .save file
-    shutil.copyfile(EXPORT_SAVE_PATH, save_file) # overwrite the .save file
-    
+        os.makedirs(EXPORT_DIR_PATH)
+    path_in = IMPORT_DIR_PATH # path of imported files directory
+    path_out = EXPORT_SAVE_PATH # path of temporary .save file to create
+    compress_save(path_in, path_out) # compress to a temporary .save file
+    validate_save(path_in, path_out) # ensure that the created 
+    shutil.copyfile(path_out, save_file) # overwrite "save_file"
+
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Modify Barotrauma .save files.')
-    parser.add_argument('operation', type=str, choices=OPERATION_STRINGS, help='|'.join(OPERATION_STRINGS))
-    parser.add_argument('save_file', type=str, help='.save file to open')
+    parser.add_argument('operation', type=str, choices=OPERATION_STRINGS, help="Import from or export to a .save file.")
+    parser.add_argument('save_file', type=str, help='A .save file to open.')
     args = parser.parse_args()
 
-    operation : str = args.operation
-    save_file : str = args.save_file
+    operation = args.operation
+    save_file = args.save_file
 
     if not os.path.isfile(save_file):
         print(f'File "{save_file}" does not exist')
@@ -96,12 +142,29 @@ def main(argv):
         os.mkdir(TEMPORARY_FILES_PATH) # create TEMPORARY_FILES_PATH if it does not exist
 
     if operation == 'import':
-        import_save(save_file)
+        import_save(save_file) # import from a .save
     elif operation == 'export':
-        export_save(save_file)
+        export_save(save_file) # export to a .save
     
     return 0
 
+
 if __name__ == "__main__":
-    retcode = main(sys.argv)
-    sys.exit(retcode)
+    if len(sys.argv) == 1:
+        # without command-line args (prompt for parameters)
+        try:
+            print("import or export:")
+            operation = input()
+            print("file path:")
+            save_file = input().strip("\"")
+            if operation == 'import':
+                import_save(save_file)
+            elif operation == 'export':
+                export_save(save_file)
+        except Exception as e:
+            logging.error('Error at %s', 'division', exc_info=e)
+            input()
+    else:
+        # with command-line args
+        retcode = main(sys.argv)
+        sys.exit(retcode)
